913bc4babadf63e4aa5a89a9b241fa9ef0645ee7
Mardzi <marko.milevski@outlook.com>
2021-04-28 00:22:51 +0200

    5th commit
    Signature link is clickable dashboardpage/test

--- src/main/java/Pages/DashboardPage.java
+++ src/main/java/Pages/DashboardPage.java
@@ -5,10 +5,26 @@
  */
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 /**
  *
  * @author kromp
  */
 public class DashboardPage {
+    //Lokatori
+       private static WebDriver driver;
+       By navSignatureLink = By.cssSelector("ul:first-of-type>li:nth-of-type(2)>a");
+    //Konstruktori
+
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
+    }
     
+    
+    //Metode
+    public void clickOnSignatureNavLink(){
+        driver.findElement(navSignatureLink).click();
+    }
 }
--- src/test/java/admin/DashboardTest.java
+++ src/test/java/admin/DashboardTest.java
@@ -11,18 +11,27 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.DashboardPage;
+import pages.LoginPage;
 
 /**
  *
  * @author kromp
  */
 public class DashboardTest {
-    
+    private static WebDriver driver;
     public DashboardTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        System.setProperty("webdriver.chrome.driver","c:\\chromedriver_90.exe");
+        DashboardPage dashboardpage = new DashboardPage(driver);
+        driver = new ChromeDriver();
     }
     
     @AfterClass
@@ -31,15 +40,21 @@ public class DashboardTest {
     
     @Before
     public void setUp() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.pageload("http://bvtest.school.cubes.rs/login");
+        loginPage.enterEmail("qa@cubes.rs");
+        loginPage.enterPassword("cubesqa");
+        loginPage.clickOnLoginButton();
     }
     
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testSignatureNavLink(){
+//       WebElement signature = driver.findElement(By.xpath("//*[@id=\"app-navbar-collapse\"]/ul[1]/li[2]/a")); 
+       DashboardPage dashboardPage = new DashboardPage(driver);
+       dashboardPage.clickOnSignatureNavLink();
+}
 }
